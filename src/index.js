40f90d10bf58fb48cf1e22f19d8fec405d6ea330
ryanboris <ryan.boris@gmail.com>
2019-03-21 16:25:38 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext, useReducer, useState } from 'react'
+import { render } from 'react-dom'
+import uuidv4 from 'uuid'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PostsContext = React.createContext({
+  posts: [ { id: 0, title: 'test post', content: 'test test test test' } ]
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function reducer(state, action) {
+  if (action.type === 'ADD POST') {
+    return {
+      ...state,
+      posts: [ ...state.posts, action.payload ]
+    }
+  }
+}
+
+function Posts() {
+  const [ title, setTitle ] = useState('')
+  const [ content, setContent ] = useState('')
+  const { state, dispatch } = useContext(PostsContext)
+  console.log(state)
+  return (
+    <div>
+      {state.posts.map(post => (
+        <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.content}</p>
+        </div>
+      ))}
+
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          dispatch({
+            type: 'ADD POST',
+            payload: { title, content, id: uuidv4() }
+          })
+          setTitle('')
+          setContent('')
+        }}
+      >
+        <input
+          type="text"
+          placeholder="enter title"
+          name="title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="enter title"
+          name="content"
+          value={content}
+          onChange={e => setContent(e.target.value)}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+
+function App() {
+  const initialState = useContext(PostsContext)
+  const [ state, dispatch ] = useReducer(reducer, initialState)
+  return (
+    <PostsContext.Provider value={{ state, dispatch }}>
+      <Posts />
+    </PostsContext.Provider>
+  )
+}
+
+render(<App />, document.getElementById('root'))
